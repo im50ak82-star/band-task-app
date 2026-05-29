90c5fa9fddab5c40a58664e94f0edb3953afdfd1
IM50AK82-Star <im50.ak82@gmail.com>
2026-05-30 00:43:39 +0900

    fix nested git

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+import {
+getFirestore,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBrDKn38T-0fFSpds5PSY7BSV0JxJa5h0M",
+  authDomain: "fpwe-tasks.firebaseapp.com",
+  projectId: "fpwe-tasks",
+  storageBucket: "fpwe-tasks.firebasestorage.app",
+  messagingSenderId: "959333591854",
+  appId: "1:959333591854:web:f35c049f9d2fcd620328bf",
+  measurementId: "G-KSMJSGLG37"
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+export const db =
+getFirestore(app);
