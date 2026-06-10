8dfe4e5dc59a31c407c6f969fde5d9426f761526
IM50AK82-Star <im50.ak82@gmail.com>
2026-06-10 23:14:38 +0900

    login12

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,31 @@
 
 import { auth } from "../../firebase";
 
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+
 import {
 GoogleAuthProvider,
 signInWithRedirect,
 } from "firebase/auth";
 
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
+const router = useRouter();
+
+useEffect(() => {
+return onAuthStateChanged(auth, (user) => {
+console.log("login page user:", user);
+
+if (user) {
+router.push("/");
+}
+});
+}, [router]);
+
 const login = async () => {
 try {
-alert("押された");
 
 const provider =
 new GoogleAuthProvider();
